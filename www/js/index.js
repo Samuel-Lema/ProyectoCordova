61398cae8ac21d33c7c00e25e3cef29f4561d750
samuel-lema <thundersoulgamer@gmail.com>
2019-03-30 12:49:30 +0100

    He añadido el plugin del escaner para que se ejecute al pulsar el boton.

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ var app = {
 
     onDeviceReady: function() {        
 
-        // Evento que se ejecuta cuando pulsas en el Boton del Escaner, permite escanear codigos de barras y QR
+        // Evento que se ejecuta cuando pulsas en el Boton de la Camara, permite sacar fotos y mostral la última en pantalla
 
         document.getElementById('btnCam').addEventListener('click', () => {
 
@@ -17,6 +17,29 @@ var app = {
             });
         });
 
+        // Evento que se ejecuta cuando pulsas en el Boton del Escaner, permite escanear codigos de barras y QR
+        // Para que funcione he tenido que cambiar la versdion de android a 6.3.0 debido a que solo es compatible con la 6.3.0 o menor
+        // Y he añadido unas lineas en el config.xml (en la carpeta raiz):
+        //
+        // <config-file target="AndroidManifest.xml" parent="/*" mode="merge">
+        //      <uses-permission android:name="android.permission.CAMERA" />
+        //      <uses-feature android:name="android.hardware.camera" />
+        //      <uses-feature android:name="android.hardware.camera.autofocus" />
+        // </config-file>
+
+        document.getElementById('btnScan').addEventListener('click', () => {
+
+            cordova.plugins.barcodeScanner.scan(
+
+                function (result) {
+                    alert("Barcode Scanner \n" + "Resultado: " + result.text + "\n" + "Formato: " + result.format + "\n" + "Cancelado: " + result.cancelled);
+                },
+
+                function (error) {
+                    alert("Escaneo fallado: " + error);
+                }
+            );
+        });
     },
 
     onSuccess: function(imageData) {
